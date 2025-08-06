07887e73d8c00ae055e104e0d73edff1b5edeea2
alliewww <aa2993.c@gmail.com>
2025-08-06 12:47:40 +0900

    fix check map cell

--- src/map/map_validate.c
+++ src/map/map_validate.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   map_validate.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: mamiyaza <mamiyaza@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*   By: lchuang <lchuang@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/04 17:12:03 by lchuang           #+#    #+#             */
-/*   Updated: 2025/08/06 12:03:23 by mamiyaza         ###   ########.fr       */
+/*   Updated: 2025/08/06 12:46:39 by lchuang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ static int	validate_map_cell(t_game *game, int y, int x, int *player_cnt)
 	char	cell;
 
 	cell = game->map[y][x];
+	if (!ft_strchr("01NSWE ", cell))
+		return (put_errmsg("Invalid character in map"));
 	if (ft_strchr("NSWE", cell))
 		(*player_cnt)++;
 	if (ft_strchr("0NSWE", cell))
@@ -32,8 +34,6 @@ static int	validate_map_cell(t_game *game, int y, int x, int *player_cnt)
 		if (game->map[y][x - 1] == ' ' || game->map[y][x + 1] == ' ')
 			return (put_errmsg("Map must be enclosed by walls"));
 	}
-	else if (cell != '1' && cell != ' ')
-		return (put_errmsg("Invalid character in map"));
 	return (1);
 }
 
